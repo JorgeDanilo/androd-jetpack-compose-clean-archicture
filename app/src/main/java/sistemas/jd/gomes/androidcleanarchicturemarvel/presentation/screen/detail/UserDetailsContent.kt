945afb0fb71ae47536a510209393cd589a0d4ce2
Jorge Danilo <sobrejavatec@gmail.com>
2022-06-20 16:05:10 -0300

    change detail view

--- app/src/main/java/sistemas/jd/gomes/androidcleanarchicturemarvel/presentation/screen/detail/UserDetailsContent.kt
+++ app/src/main/java/sistemas/jd/gomes/androidcleanarchicturemarvel/presentation/screen/detail/UserDetailsContent.kt
@@ -4,67 +4,115 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import coil.size.Scale
-import sistemas.jd.gomes.androidcleanarchicturemarvel.ui.theme.AppThemColor
 import sistemas.jd.gomes.domain.model.User
+import sistemas.jd.gomes.androidcleanarchicturemarvel.R
 
 @Composable
 fun UserDetailsContent(user: User) {
+
     val scrollState = rememberScrollState()
-    Card(
-        elevation = 0.dp,
-        backgroundColor = MaterialTheme.colors.AppThemColor
-    ) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .verticalScroll(scrollState)
-        ) {
 
-            Image(
-                painter = rememberImagePainter(
-                    data = user.avatar, builder = {
-                        crossfade(true)
-                        scale(Scale.FIT)
-                    }),
-                contentDescription = null,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(350.dp)
-            )
+    Column(modifier = Modifier.fillMaxSize()) {
 
-            Column(Modifier.padding(8.dp)) {
-                Spacer(modifier = Modifier.height(16.dp))
-                user.firstName?.let {
-                    Text(
-                        text = it,
-                        style = MaterialTheme.typography.h5,
-                        fontWeight = FontWeight.Bold
-                    )
-                }
-                Spacer(modifier = Modifier.height(8.dp))
-                user.lastName?.let {
-                    Text(
-                        text = it,
-                        style = MaterialTheme.typography.body2,
-                    )
-                }
-                Spacer(modifier = Modifier.height(8.dp))
-                user.email?.let {
-                    Text(
-                        text = it,
-                        style = MaterialTheme.typography.body2
-                    )
+        BoxWithConstraints {
+            Surface {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .verticalScroll(scrollState),
+                ) {
+                    UserHeader(user, this@BoxWithConstraints.maxHeight)
+                    UserContent(user, this@BoxWithConstraints.maxHeight)
                 }
             }
         }
+
     }
+}
+
+@Composable
+fun UserContent(user: User, containerHeight: Dp) {
+    Column {
+        Spacer(modifier = Modifier.heightIn(8.dp))
+        Name(user)
+        user.lastName?.let { UserProperty(stringResource(R.string.lastName), it) }
+        user.email?.let { UserProperty(label = stringResource(R.string.email), value = it) }
+
+        Spacer(modifier = Modifier.height((containerHeight - 320.dp).coerceAtLeast(0.dp)))
+    }
+}
+
+@Composable
+fun UserProperty(label: String, value: String, isLink: Boolean = false) {
+    Column(modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)) {
+        Divider()
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text(
+                text = label,
+                modifier = Modifier.paddingFromBaseline(24.dp),
+                style = MaterialTheme.typography.caption
+            )
+        }
+        val style = if (isLink) {
+            MaterialTheme.typography.body1.copy(color = MaterialTheme.colors.primary)
+        } else {
+            MaterialTheme.typography.body1
+        }
+
+        Text(
+            text = value,
+            modifier = Modifier.paddingFromBaseline(24.dp),
+            style = style
+        )
+
+    }
+}
+
+@Composable
+private fun Name(user: User) {
+    Column(modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)) {
+        Name(
+            user = user,
+            modifier = Modifier.paddingFromBaseline(32.dp)
+        )
+    }
+}
+
+@Composable
+private fun Name(user: User, modifier: Modifier = Modifier) {
+    user.firstName?.let {
+        Text(
+            text = it,
+            modifier = modifier,
+            style = MaterialTheme.typography.h5,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun UserHeader(user: User, containerHeight: Dp) {
+    Image(
+        modifier = Modifier
+            .heightIn(max = containerHeight / 2)
+            .fillMaxWidth(),
+        painter = rememberImagePainter(
+            data = user.avatar, builder = {
+                crossfade(true)
+                scale(Scale.FIT)
+            }),
+        contentScale = ContentScale.Crop,
+        contentDescription = null
+    )
 }
