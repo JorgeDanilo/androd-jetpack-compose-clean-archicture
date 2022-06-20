b7cca90044f477ca5c3adc1bc45e6b591e64de46
Jorge Danilo <sobrejavatec@gmail.com>
2022-06-20 14:55:27 -0300

    add state view

---
+++ app/src/main/java/sistemas/jd/gomes/androidcleanarchicturemarvel/presentation/State.kt
@@ -0,0 +1,11 @@
+package sistemas.jd.gomes.androidcleanarchicturemarvel.presentation
+
+sealed class State<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class START<T> : State<T>()
+    class SUCCESS<T>(data: T) : State<T>(data)
+    class ERROR<T>(message: String, data: T? = null) : State<T>(data, message)
+    class LOADING<T> : State<T>()
+}
